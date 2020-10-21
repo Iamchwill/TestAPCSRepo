0cb79d21072613b2632d9f36acf6bfbe81def426
A2c4u6t <wchen20@stuy.edu>
2020-10-21 12:22:44 -0400

    Added 2 methods for converting array to String and they work

--- Demo.java
+++ Demo.java
@@ -1,11 +1,23 @@
 public class Demo {
   public static void main(String[] args) {
-    if (args.length > 0) {
+    int[] one = {1};
+    int[] two = {1,2};
+    int[] three = {1,2,3};
+    int[] four = {1,2,3,4};
+    int[] zero = {};
+    int[][] fill = new int[5][];
+    fill[0] = zero;
+    fill[1] = one;
+    fill[2] = two;
+    fill[3] = three;
+    fill[4] = four;
+    System.out.println(arrayDeepToString(fill));
+    /* if (args.length > 0) {
       printLoop(Integer.parseInt(args[0]));
     }
     else {
       printLoop(5);
-    }
+    } */
   }
 
   public static void printLoop(int x) {
@@ -16,4 +28,28 @@ public class Demo {
       System.out.println();
     }
   }
+
+  public static String arrToString(int[] arr) {
+    String out = "{";
+    for (int i = 0; i < arr.length; i++) {
+      if (i == arr.length - 1) {
+        out += arr[i];
+      }
+      else {
+        out += arr[i] + ", ";
+      }
+    }
+    return out += "}";
+   }
+
+   public static String arrayDeepToString(int[][] arr) {
+     String out = "{";
+     for (int col = 0; col < arr.length; col++) {
+       if (col == arr.length - 1) {
+         out += arrToString(arr[col]);
+       }
+       else out += arrToString(arr[col]) + ", ";
+     }
+       return out += "}";
+     }
 }
