5656d0c1e1fd678d4058d07c33b79d51f2c2562a
A2c4u6t <wchen20@stuy.edu>
2020-10-21 14:43:28 -0400

    Made my testing comments so it won't output when other people use it

--- Demo.java
+++ Demo.java
@@ -1,18 +1,21 @@
 public class Demo {
   public static void main(String[] args) {
-    int[] one = {1};
-    int[] two = {1,2};
-    int[] three = {1,2,3};
-    int[] four = {1,2,3,4};
-    int[] zero = {};
-    int[][] fill = new int[5][];
-    fill[0] = zero;
-    fill[1] = one;
-    fill[2] = two;
-    fill[3] = three;
-    fill[4] = four;
-    System.out.println(arrayDeepToString(fill));
-    /* if (args.length > 0) {
+    /* Tests
+      int[] one = {1};
+      int[] two = {1,2};
+      int[] three = {1,2,3};
+      int[] four = {1,2,3,4};
+      int[] zero = {};
+      int[][] fill = new int[5][];
+      fill[0] = zero;
+      fill[1] = one;
+      fill[2] = two;
+      fill[3] = three;
+      fill[4] = four;
+      System.out.println(arrayDeepToString(fill));
+      System.out.println(arrayDeepToString(create2DArrayRandomized(10,10,10)));
+      System.out.println(arrayDeepToString(create2DArray(3,3,3)));
+      if (args.length > 0) {
       printLoop(Integer.parseInt(args[0]));
     }
     else {
@@ -44,12 +47,26 @@ public class Demo {
 
    public static String arrayDeepToString(int[][] arr) {
      String out = "{";
-     for (int col = 0; col < arr.length; col++) {
-       if (col == arr.length - 1) {
-         out += arrToString(arr[col]);
+     for (int row = 0; row < arr.length; row++) {
+       if (row == arr.length - 1) {
+         out += arrToString(arr[row]);
        }
-       else out += arrToString(arr[col]) + ", ";
+       else out += arrToString(arr[row]) + ", ";
      }
        return out += "}";
+     }
+
+     public static int[][] create2DArray(int rows, int cols, int maxVal) {
+       int[][] out = new int[rows][cols];
+       for (int i = 0; i < rows; i++) {
+         for (int j = 0; j < cols; j++) {
+           out[i][j] = (int)(Math.random() * (maxVal + 1));
+         }
+       }
+       return out;
+     }
+
+     public static int[][] create2DArrayRandomized(int rows, int cols, int maxVal) {
+        return create2DArray((int)(Math.random() * (rows + 1)), (int)(Math.random() * (cols + 1)), maxVal);
      }
 }
